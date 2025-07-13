ffefcd0a72b4e386e4acb5842d8580ae49b60058
Leonardo <lnrdmadeira@gmail.com>
2025-07-13 04:04:17 -0300

    feat: atividade 4 finalizada

--- Atividade-4.py
+++ Atividade-4.py
@@ -10,98 +10,27 @@ def subRetangulos(qnt_linhas, qnt_colunas, perimetro_max):
                 if altura <= qnt_linhas and largura <= qnt_colunas:      
                     canditados_wh.append((largura, altura))
     
-    print(canditados_wh)
     return canditados_wh
 
-def testesHorizontais(perimetro_max, qnt_linhas, qnt_colunas, mapa):
-    possibilidades = subRetangulos(qnt_linhas, qnt_colunas, perimetro_max)
+def encontrarTesouros(perimetro_max, qnt_linhas, qnt_colunas, mapa):
+    possibilidades = subRetangulos(qnt_linhas, qnt_colunas, perimetro_max)    
 
-    matrizes_possiveis = []
-    tesouros_encontrados = []
+    max_tesouros = 0
 
+    for largura, altura in possibilidades:
+        for linha in range(qnt_linhas - altura + 1):
+            for coluna in range(qnt_colunas - largura + 1):  
+                tesouros = 0
+                for x in range(linha, linha + altura):
+                    for y in range(coluna, coluna + largura):
+                        if mapa[x][y] == 'X':
+                            tesouros += 1
+                max_tesouros = max(max_tesouros, tesouros)
     
-    # for possibilidade in possibilidades:
-    #     for altura in range(possibilidade[1]):
-    #     # if possibilidade[0] > possibilidade[1]:
-    #         index = 0
-    #         while index <= qnt_colunas - possibilidade[0]:
-    #             for linha in mapa:
-    #                 tesouros = 0
-    #                 for posicao in range(index, possibilidade[0] + index):
-    #                     if linha[posicao] == 'X':
-    #                         tesouros += 1
-    #                 tesouros_encontrados.append(tesouros)
-    #             index += 1
-    # print(tesouros_encontrados)
-
-    # for possibilidade in possibilidades:
-    #     altura = possibilidade[1]
-    #     largura = possibilidade[0]
-    #     print("Largura: ", largura)
-    #     print("Altura: ", altura)
-    #     maximo = (qnt_linhas - (altura - 1))
-
-    #     ##### Horizontal
-    #     # if largura > altura:
-    #     for y in range(altura):
-    #         tesouros = 0
-
-    #         for linha in mapa[y:(maximo + y)]:
-
-    #             ## FAZER MAIS UM LOOP AQUI DENTRO PERCORRENDO O SLICE DA HORIZONTAL AGORA
-    #             for posicao in linha:
-    #                 if posicao == 'X':
-    #                     tesouros += 1
-    #         tesouros_encontrados.append(tesouros)
-    #     print(tesouros_encontrados)
-
-    # for possibilidade in possibilidades:
-    #     altura = possibilidade[1]
-    #     largura = possibilidade[0]
-    #     maximo_altura = (qnt_linhas - altura)
-    #     maximo_largura = (qnt_colunas - largura)
-
-    #     altura_atual = 0
-    #     while altura_atual <= maximo_altura:
-    #         for y in range(maximo_altura):
-    #             for x in range(largura):
-    #                 # if mapa[x][y] == "X":
-    #                 #     print("X")
-    #                 # print(mapa[x][y], end=" ")
-
-    #                 for linha in mapa[altura_atual: (altura + y)]
-    #             print()
-    #         altura_atual += 1
-
-    for possibilidade in possibilidades:
-        altura = possibilidade[1]
-        largura = possibilidade[0]
-        maximo_altura = (qnt_linhas - altura)
-        maximo_largura = (qnt_colunas - largura)
-                        
-        altura_atual = 0
-        while altura_atual <= maximo_altura:
-            matrix_temp = []
-            for index in range(largura):
-                matrix_temp.append(mapa[altura_atual][index: largura + index])
-            matrizes_possiveis.append(matrix_temp)
-            
-
-            altura_atual += 1
-        print(matrizes_possiveis)
-
         
+    return max_tesouros
 
-
-
-        # for indexY in range(len(matriz)):
-            # for indexX in range(len(matriz)):
-                # print(matriz[indexX][indexY], end=" ")
-            # print()
-
-
-qnt_linhas = int(input("Quantidade de linhas: "))
-qnt_colunas = int(input("Quantidade de colunas: "))
+qnt_linhas, qnt_colunas = [int(i) for i in input().split()]
 
 controle_linhas = qnt_linhas
 
@@ -117,4 +46,4 @@ while controle_linhas:
 
 perimetro_max = int(input("Insira o perimetro máximo: "))
 
-testesHorizontais(perimetro_max, qnt_linhas, qnt_colunas, mapa)
+print(encontrarTesouros(perimetro_max, qnt_linhas, qnt_colunas, mapa))
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# atividade-final-algoritmos
+# Atividade final: Algoritmos
+
+Este repositório é referente à atividade final da disciplina de Algoritmos do curso de Ciência de Dados e Inteligência Artificial da Universidade Estadual de Londrina (UEL).
+
+## Sobre o repositório
+
+Aqui estão reunidas as soluções das atividades propostas na disciplina, cada uma abordando diferentes conceitos fundamentais de algoritmos e programação em Python. O objetivo é aplicar os conhecimentos adquiridos ao longo do curso, resolvendo problemas práticos e desenvolvendo habilidades de lógica computacional.
+
+## Estrutura
+
+- `Atividade-1.py`: Solução da primeira atividade.
+- `Atividade-2.py`: Solução da segunda atividade.
+- `Atividade-3.py`: Solução da terceira atividade.
+- `Atividade-4.py`: Solução da quarta atividade.
+- `Atividade-Final.pdf`: Pdf com os enunciados das questões.
+
+
+Cada arquivo contém o código-fonte correspondente à atividade, com comentários explicativos para facilitar o entendimento.
+
+Soluções desenvolvidas por:<br>
+Leonardo Madeira Alves Pereira
